58a3f13705089c0ada1f62d0eadd732594e5ad7b
yaodao0520 <yaodao456@gmail.com>
2025-03-28 19:08:36 +0800

    Site updated: 2025-03-28 19:08:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/28/hello-world/","2025/03/28/测试页/"];function toRandomPost(){
+var posts=["2025/03/28/测试页/","2025/03/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
