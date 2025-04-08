22ec80fd9fc8d0da1626217f30df712ed2b34259
yaodao0520 <yaodao456@gmail.com>
2025-04-08 15:53:53 +0800

    Site updated: 2025-04-08 15:53:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/测试一/","undefined/测试页/"];function toRandomPost(){
+var posts=["undefined/这是一篇新的博文/","undefined/测试一/","undefined/测试页/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
