4dbfbf7d85b2710f390a8e6c37debeec194034c4
yaodao0520 <yaodao456@gmail.com>
2025-04-19 15:29:29 +0800

    add_branch

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["undefined/云端测试页面/","undefined/这是一篇新的博文/","undefined/测试一/","undefined/测试页/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
