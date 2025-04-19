4fbd676c897a8ec78f5fcab0ec1ad9117fc1588f
yaodao0520 <yaodao456@gmail.com>
2025-04-19 18:35:13 +0800

    Add files via upload

---
+++ public/anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["undefined/云端测试页面/","undefined/这是一篇新的博文/","undefined/测试一/","undefined/测试页/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
