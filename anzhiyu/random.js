30548e77e3c9d94d288623ba26ddcd8fd8db65e4
yaodao0520 <yaodao456@gmail.com>
2025-04-25 14:00:49 +0800

    Site updated: 2025-04-25 14:00:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/大师兄影视/","undefined/NekoBox/","undefined/软件仓库/","undefined/学习网站/"];function toRandomPost(){
+var posts=["undefined/笔趣书阁/","undefined/大师兄影视/","undefined/NekoBox/","undefined/软件仓库/","undefined/学习网站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
