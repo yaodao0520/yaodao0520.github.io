7e5e1df25db80b3756df0ba6ffe821050fe8f39c
yaodao0520 <yaodao456@gmail.com>
2025-04-29 19:52:13 +0800

    Site updated: 2025-04-29 19:52:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/回归-时隔180天，我们归来并全新上线！/","undefined/主播视频/","undefined/Spotify/","undefined/笔趣书阁/","undefined/大师兄影视/","undefined/NekoBox/","undefined/软件仓库/","undefined/学习网站/"];function toRandomPost(){
+var posts=["undefined/回归-时隔180天，我们归来并全新上线！/","undefined/测试/","undefined/一篇测试/","undefined/这是一篇测试/","undefined/主播视频/","undefined/Spotify/","undefined/笔趣书阁/","undefined/大师兄影视/","undefined/NekoBox/","undefined/软件仓库/","undefined/学习网站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
